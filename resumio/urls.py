8e5dcf401edc45bfb0f7a6ed586f7d78818410a6
zahra <sa.zahrasalehi@gmail.com>
2024-01-18 15:30:43 +0330

    I should've known better than to cheat a friend

--- resumio/urls.py
+++ resumio/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('/', include('thisIsWhereUShouldTakeAlook.urls')),
+
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
-
 ]
