db682db805b5487a84fba7613b9a65bf51097b1b
zahra <sa.zahrasalehi@gmail.com>
2024-01-18 17:04:40 +0330

    Guilty feet have got no rhythm

--- resumio/urls.py
+++ resumio/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('/', include('thisIsWhereUShouldTakeAlook.urls')),
 
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
+    path('resume/', include('thisIsWhereUShouldTakeAlook.urls')),
+
 ]
